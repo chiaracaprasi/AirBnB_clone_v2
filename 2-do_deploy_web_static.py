91fdc59462b8db3109d18acae5f166a4bc6c7614
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-25 16:01:20 +1000

    fix: path name

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -41,7 +41,7 @@ def do_deploy(archive_path):
         f_name_no_ext = f_name.split(".")[0]
         put(archive_path, '/tmp')
         run('mkdir -p {}{}/'.format(path, f_name_no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(f_name, path, f_name_no_ext))
+        run('tar -xzf /tmp/{} -C {}{}'.format(f_name, path, f_name_no_ext))
         run('rm /tmp/{}'.format(f_name))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, f_name_no_ext))
         run('rm -rf {}{}/web_static'.format(path, f_name_no_ext))
