18a1abfb53b6a29bdcb61cb7052c4496a1ce43cd
Chiara Caprasi <caprasichiara@gmail.com>
2022-08-25 15:38:54 +1000

    fix: change to format string due to python v3.4

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,10 +5,7 @@ sing the function do_pack. """
 
 from fabric.api import *
 from datetime import datetime
-from os import path
-
-env.user = 'ubuntu'
-env.hosts = ['52.23.231.242', '34.229.134.248']
+from os.path
 
 
 def do_pack():
@@ -16,9 +13,9 @@ def do_pack():
         Generates a .tgz archine from contents of web_static
     """
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"versions/web_static_{date}.tgz"
+    filename = "versions/web_static_{}.tgz".format(date)
     local("mkdir -p versions")
-    archive_file = local(f"tar -czvf {filename} web_static")
+    archive_file = local("tar -czvf {} web_static".format(filename))
     if archive_file:
         return filename
     else:
@@ -35,16 +32,14 @@ def do_deploy(archive_path):
         return False
 
     else:
-        # Upload the archive to the /tmp/ directory of the web server
         path = '/data/web_static/releases/'
         f_name = archive_path.split("/")[-1]
         f_name_no_ext = f_name.split(".")[0]
-
         put(archive_path, '/tmp')
-        run(f'mkdir -p /data/web_static/releases/{f_name}')
-        run(f'tar -xzf /tmp/{f_name} -C {path}{f_name_no_ext}/')
-        run(f'rm /tmp/{f_name}')
-        run(f'mv {path}{f_name_no_ext}/web_static/* {path}{f_name_no_ext}/')
-        run(f'rm -rf {path}{f_name_no_ext}/web_static')
+        run('mkdir -p {}{}/'.format(path, f_name_no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(f_name, path, f_name_no_ext))
+        run('rm /tmp/{}'.format(f_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, f_name_no_ext))
+        run('rm -rf {}{}/web_static'.format(path, f_name_no_ext))
         run('rm -rf /data/web_static/current')
-        run(f'ln -sf {path}{f_name_no_ext} /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, f_name_no_ext))
