94927b9873bf5dc2b853be7362d87eafa7669f33
Chiara Caprasi <caprasichiara@gmail.com>
2022-09-14 16:01:09 +1000

    build: add task 0 - 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,5 +14,17 @@ def hello():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_text(text):
+    """returns C followed by value of text"""
+    return f'C ' + text.replace("_", " ")
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
